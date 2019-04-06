3c65b4dc2f274fb1c7827d9bf4af22d3ecf7594a
Mikhail Lazarev <mikael.lazarev@gmail.com>
2019-04-06 18:49:04 +0300

    Testing heroku

--- heroku.sh
+++ heroku.sh
@@ -2,12 +2,12 @@ cd server
 python manage.py migrate
 python manage.py collectstatic
 
-ENV DJANGO_DB_NAME=default
-ENV DJANGO_SU_NAME=admin
-ENV DJANGO_SU_EMAIL=admin@my.company
-ENV DJANGO_SU_PASSWORD=mypass
+export DJANGO_DB_NAME=default
+export DJANGO_SU_NAME=admin
+export DJANGO_SU_EMAIL=admin@my.company
+export DJANGO_SU_PASSWORD=mypass
 
-RUN python -c "import django; django.setup(); \
+python -c "import django; django.setup(); \
    from django.contrib.auth.management.commands.createsuperuser import get_user_model; \
    get_user_model()._default_manager.db_manager('$DJANGO_DB_NAME').create_superuser( \
    username='$DJANGO_SU_NAME', \
