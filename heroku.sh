e17350e0d27bf375c05f3e942c818d0906102c5a
Mikhail Lazarev <mikael.lazarev@gmail.com>
2019-04-06 18:47:12 +0300

    Testing heroku

--- heroku.sh
+++ heroku.sh
@@ -1,5 +1,18 @@
 cd server
 python manage.py migrate
 python manage.py collectstatic
+
+ENV DJANGO_DB_NAME=default
+ENV DJANGO_SU_NAME=admin
+ENV DJANGO_SU_EMAIL=admin@my.company
+ENV DJANGO_SU_PASSWORD=mypass
+
+RUN python -c "import django; django.setup(); \
+   from django.contrib.auth.management.commands.createsuperuser import get_user_model; \
+   get_user_model()._default_manager.db_manager('$DJANGO_DB_NAME').create_superuser( \
+   username='$DJANGO_SU_NAME', \
+   email='$DJANGO_SU_EMAIL', \
+   password='$DJANGO_SU_PASSWORD')"
+
 ls -la
 gunicorn settings.wsgi
