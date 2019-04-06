ea46ad6351882e9649d2fa4c3c2a61a23ef485b2
Mikhail Lazarev <mikael.lazarev@gmail.com>
2019-04-06 21:48:16 +0300

    Add corsheads to settings

--- settings/settings_dev.py
+++ settings/settings_dev.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'orders.apps.OrdersConfig',
     'tickers.apps.TickersConfig',
     'markets.apps.MarketsConfig',
