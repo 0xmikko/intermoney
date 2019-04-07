9496372e6bdb1c47b2bfe62020b7c15a76cad451
Mikhail Lazarev <mikael.lazarev@gmail.com>
2019-04-07 10:30:08 +0300

    orders history added

--- orders/viewsets.py
+++ orders/viewsets.py
@@ -1,5 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.decorators import action
+from rest_framework import status
+from rest_framework.response import Response
 
 from .models import Order
 from .serializers import OrderSerializer
@@ -23,8 +25,10 @@ class OrdersViewSet(viewsets.ModelViewSet):
 
     @action(detail=False)
     def history(self, request):
-        # List last orders
-        pass
+        # Add user checking
+        queryset = Order.objects.all()[:10]
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     @action(detail=False, methods=['POST'])
     def buy_market(self, request):
